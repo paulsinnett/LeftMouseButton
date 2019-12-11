13aa75c587836736b7f8906290e2e3374a4e9acf
Paul Sinnett <paul.sinnett@gmail.com>
2019-12-11 09:24:53 +0000

    Initial commit

---
+++ Assets/LeftMouseButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LeftMouseButton : MonoBehaviour
+{
+    Text text;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+        StartCoroutine(CheckLeftMouse());
+    }
+
+    IEnumerator CheckLeftMouse()
+    {
+        while (true)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                text.enabled = true;
+                yield return new WaitForSeconds(1);
+                text.enabled = false;
+            }
+            yield return null;
+        }
+    }
+}
